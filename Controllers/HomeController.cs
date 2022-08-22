bd2261be7d324061cfeadf1aaf5beac6c0f24ddb
MikhailStukan <renewy32@gmail.com>
2022-08-22 10:35:10 +0200

    tags first release

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using CollectionsPortal.Models;
 using Microsoft.AspNetCore.Mvc;
+using CollectionsPortal.Data;
 using System.Diagnostics;
 
 namespace CollectionsPortal.Controllers
@@ -7,14 +8,17 @@ namespace CollectionsPortal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _context = db;
         }
 
         public IActionResult Index()
         {
+            ViewData["tags"] = GetTags();
             return View();
         }
 
@@ -22,6 +26,22 @@ namespace CollectionsPortal.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        private List<string> GetTags()
+        {
+            List<string> tagNames = new List<string>();
+
+            var tags = _context.Tags.ToList();
+            if (tags != null)
+            {
+                foreach (var tag in tags)
+                {
+                    tagNames.Add(tag.Name);
+                }
+            }
+            return tagNames;
         }
     }
 }
